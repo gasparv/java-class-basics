1b2f22ebe964afa2fc4792c268957d45ce5cdb0d
Vladimír Gašpar <gaspar.vlado@gmail.com>
2019-02-01 09:01:57 +0100

    Pridany Lab1

---
+++ Lab1_HelloWorld/src/sk/gasparv/Main.java
@@ -0,0 +1,17 @@
+
+//Každá štruktúrovaná aplikácia obsahuje názov balíku. Štandardne sa balík aplikácie označuje
+//top level doménou (napr. sk, com, eu, at, hu, ...) a názvom organizácie. (napr. com.example)
+package sk.gasparv;
+
+
+//Trieda main je vytvorená ako prvá trieda automaticky v šablóne konzolovej aplikácie.
+//Každá aplikácia musí mať minimálne jednu triedu
+public class Main {
+
+    //Predpis - deklarácia špeciálnej metódy main, ktorá ako vstup má zoznam parametrov zadaných v príkazovom riadku a nemá výstup
+    public static void main(String[] args) {
+
+        //Funkcia pre výpis textu do konzoly
+	System.out.print("Ahoj ahoj.");
+    }
+}
